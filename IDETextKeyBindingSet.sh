a625f026008125317f4bed55d8731267e099540e
Nasrabadi <nasrabadi17@gmail.com>
2021-06-28 10:26:23 +0430

    add Customized sh

---
+++ IDETextKeyBindingSet.sh
@@ -0,0 +1,14 @@
+ex /Applications/Xcode.app/Contents/Frameworks/IDEKit.framework/Versions/A/Resources/IDETextKeyBindingSet.plist <<eof
+5 insert
+<key>Customized</key>
+        <dict>
+            <key>Duplicate Current Line</key>
+            <string>moveToBeginningOfLine:, deleteToEndOfLine:, yank:, insertNewline:, moveToBeginningOfLine:, yank:</string>
+            <key>Duplicate Lines</key>
+            <string>selectLine:, copy:, moveToEndOfLine:, insertNewline:, paste:, deleteBackward:</string>
+            <key>Delete Line</key>
+            <string>selectLine:, deleteBackward:</string>
+        </dict>
+.
+xit
+eof
